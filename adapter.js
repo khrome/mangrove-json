024ce28ab86a9dd7031eea5ff73715c9b9591c47
Abbey Sparrow <abbey@khrome.net>
2021-10-30 21:12:34 -0600

    added saving

--- adapter.js
+++ adapter.js
@@ -41,6 +41,47 @@ Adapter.prototype.loadCollection = function(collection, name, options, cb){
     }, cb);
 }
 
+Adapter.prototype.saveCollection = function(collection, name, options, cb){
+    var lcv = 0;
+    var list;
+    this.save(name, options, function(){
+        if(!list) list = Object.keys(collection.index);
+        return list.length?collection.index[list.shift()]:null;
+    }, cb);
+}
+
+Adapter.prototype.save = function(name, options, next, cb){
+    var url = path.join(this.root, name+(this.filetype?'.'+this.filetype:''));
+    var backUrl = path.join(this.root, name+'.bak'+(this.filetype?'.'+this.filetype:''));
+    fs.rename(url, backUrl, function(err){
+        //todo: handle errors by reverting
+        var writtenOne = false;
+        var writeChain = function(callback){
+            var item = next();
+            if(item){
+                var writable = (writtenOne?",\n":"\n")+JSON.stringify(item)
+                fs.appendFile(url, writable, function(err){
+                    if(!writtenOne) writtenOne = true;
+                    writeChain(callback);
+                });
+            }else{
+                callback()
+            }
+        }
+        fs.writeFile(url, '['+"\n", function(err){
+            writeChain(function(){
+                fs.appendFile(url, ']', function(err){
+                    //TODO: some kind of optional validation
+                    fs.unlink(backUrl, function(){
+                        cb();
+                    });
+                });
+            })
+        });
+
+    });
+}
+
 module.exports = {
   Adapter : Adapter
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var should = require("chai").should();
 var Indexed = require('indexed-set');
 var MangroveJSON = require('../adapter');
+var fs = require('fs');
+var path = require('path');
 //var datasource = new Mangrove({});
 
 describe('Mangrove JSON Adapter', function(){
@@ -25,6 +27,24 @@ describe('Mangrove JSON Adapter', function(){
                   var set = new Indexed.Set(loaded, 'id');
                   set.length.should.equal(4);
                   done();
+              });
+          });
+
+          it('saves a collection into a JSON file', function(done){
+              var adapter = new MangroveJSON.Adapter({root: './test/data/'});
+              var data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'beatles.json')));
+              var loaded = new Indexed.Collection(data, 'id');
+              adapter.saveCollection(loaded, 'justatest', {}, function(){
+                  var location = path.join(__dirname, 'data', 'justatest.json');
+                   var newData = JSON.parse(fs.readFileSync(location));
+                   Object.keys(newData).should.deep.equal(Object.keys(data));
+                   Object.keys(newData).forEach(function(key){
+                       data[key].should.deep.equal(newData[key]);
+                   });
+                   //test
+                   fs.unlink(location, function(){
+                       done();
+                   });
               });
           });
     });
